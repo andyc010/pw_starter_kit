48c7ee5ef57ea343026a5cf49dd2f14ef413474f
Andy Chin <ajcuc010@gmail.com>
2024-10-17 16:10:09 -0700

    Added 1 test, fixed comments, removed 1 directory

--- nodejs/tests/nodejs_starter.spec.ts
+++ nodejs/tests/nodejs_starter.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { Usage, displayCurrentDateAndTime } from  "../support/classes/date_and_time.ts";
 
-// Log in/log out test, use expandtesting.com/login
+// Log in/log out test, using https://practice.expandtesting.com/login page
 test('Log_in_log_out', async ({ page }) => {
     await page.goto("https://practice.expandtesting.com/login");
 
@@ -27,7 +27,7 @@ test('Log_in_log_out', async ({ page }) => {
     await expect(page.getByText('You logged out of the secure area!')).toBeVisible();
 });
 
-// Form test/automation, use practice-automation.com/form-fields/ page
+// Form test/automation, using https://practice-automation.com/form-fields/ page
 test('Form_textbox_checkbox_radiobutton_dropdown_alert', async ({ page }, TestInfo) => {
     await page.goto("https://practice-automation.com/form-fields/");
 
@@ -58,11 +58,11 @@ test('Form_textbox_checkbox_radiobutton_dropdown_alert', async ({ page }, TestIn
     // Click the Submit button to submit the form
     await page.getByTestId('submit-btn').click();
 
-    // Alert here
+    // Alert here, dismiss it
     page.on('dialog', dialog => dialog.accept());
 });
 
-// Form test/automation test #2 with a slider control, use https://practice-automation.com
+// Another form test/automation with a slider control, using https://practice-automation.com
 test('Form_slider', async ({ page }) => {
     await page.goto("https://practice-automation.com/slider/");
     
@@ -76,11 +76,14 @@ test('Form_slider', async ({ page }) => {
     await expect(page.getByText('50')).toBeVisible();
 });
 
-// File upload test, using https://practice.expandtesting.com/upload
+// For this next test, can use one of the supplied files in the upload_files directory
+const upload_file_used = '20241016_csv_sample_file_upload.csv';
+
+// File upload test, using https://practice.expandtesting.com/upload page
 test('File_upload', async ({ page }) => {
     await page.goto("https://practice.expandtesting.com/upload");
 
-    // Verify the page that is needed
+    // Verify the page that is needed is present
     await expect(page.getByRole('heading', { name: 'File Uploader page for Automation Testing Practice' })).toBeVisible();
 
     // Upload the file
@@ -89,7 +92,7 @@ test('File_upload', async ({ page }) => {
     const fileChooser = await fileChooserPromise;
     // 'path' is from nodejs, see https://nodejs.org/en/learn/manipulating-files/nodejs-file-paths
     const path = require('node:path');
-    await fileChooser.setFiles(path.join('../upload_files/', '20241016_csv_sample_file_upload.csv'));
+    await fileChooser.setFiles(path.join('../upload_files/', upload_file_used));
 
     // Click the Upload button
     await page.getByTestId('file-submit').click();
@@ -98,4 +101,17 @@ test('File_upload', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'File Uploaded!' })).toBeVisible();
 });
 
-// Table with data test, using 
+// Table with data test, using https://practice-automation.com/tables/ page
+test('Tables', async ({ page }) => {
+    await page.goto("https://practice-automation.com/tables/");
+
+    // Verify that the page that is wanted is appearing
+    await expect(page.getByRole('heading', { name: 'Tables' })).toBeVisible();
+
+    // Using the sortable table, press the Next link
+    await page.getByText('Next').click();
+
+    // Verify the information on the first row (11th entry/rank with country = Ethiopia, population = 126.5 million) is present
+    await expect(page.locator('td', { hasText: 'Ethiopia' })).toBeVisible();
+    await expect(page.locator('td', { hasText: '126.5'})).toBeVisible();
+});
